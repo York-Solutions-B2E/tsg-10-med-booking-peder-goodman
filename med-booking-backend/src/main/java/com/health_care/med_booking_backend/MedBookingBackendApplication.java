59a494c2be8529db7dcacf0de6bd41738b84edb6
Peder Goodman <peder.goodman@gmail.com>
2024-11-27 16:51:13 -0600

    initial commit springboot backend boilerplate

---
+++ med-booking-backend/src/main/java/com/health_care/med_booking_backend/MedBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.health_care.med_booking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedBookingBackendApplication.class, args);
+	}
+
+}

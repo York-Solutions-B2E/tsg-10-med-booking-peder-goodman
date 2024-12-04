738fa06ad6b958fa619ea724eccb2d19deb44fbb
Peder Goodman <peder.goodman@gmail.com>
2024-12-04 16:00:12 -0600

    remove navigate from patient logout button

--- med-booking-frontend/src/Components/buttons/AdminLogoutButton.tsx
+++ med-booking-frontend/src/Components/buttons/AdminLogoutButton.tsx
@@ -1,14 +1,10 @@
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
 import { logoutUser } from "../../store/actions/userActions";
 import { store } from "../../store/store";
 
 export const AdminLogoutButton = () => {
-  const navigate = useNavigate();
-
   const handleLogout = () => {
     store.dispatch(logoutUser());
-    navigate("/home");
   };
 
   return (

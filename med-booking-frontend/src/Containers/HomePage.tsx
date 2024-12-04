49a665db9cf14d3e16df44430c64b3b69e92c870
Peder Goodman <peder.goodman@gmail.com>
2024-12-04 14:40:29 -0600

    add base of pages and forms

---
+++ med-booking-frontend/src/Containers/HomePage.tsx
@@ -0,0 +1,11 @@
+// aka login
+
+export const HomePage = () => {
+  return (
+    <>
+      <div>HomePage</div>
+      <div>Looks like you're not logged in!!</div>
+      <div>Please Login!</div>
+    </>
+  );
+};

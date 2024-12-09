150ddd24c2bc88a602fa802533113fe9e2a65859
Peder Goodman <peder.goodman@gmail.com>
2024-12-09 10:28:03 -0600

    add AppointmentService move logic and add sudo code to Service layer

--- med-booking-backend/src/main/java/com/health_care/med_booking_backend/controller/AppointmentController.java
+++ med-booking-backend/src/main/java/com/health_care/med_booking_backend/controller/AppointmentController.java
@@ -8,25 +8,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.health_care.med_booking_backend.dto.AppointmentRequestDTO;
+import com.health_care.med_booking_backend.model.Appointment;
+import com.health_care.med_booking_backend.service.AppointmentService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/appointments")
 public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
 
     @PostMapping("/create")
-    public ResponseEntity<String> createNewAppointment(@RequestBody AppointmentRequestDTO appointmentRequestDTO) {
-        return ResponseEntity.ok("Appointment Created!");
+    public ResponseEntity<String> createNewAppointment(@RequestBody Appointment appointment) {
+        return appointmentService.createNewAppointment(appointment);
     }
 
     @PutMapping("/update/details")
-    public ResponseEntity<String> updateAppointmentDetails(@RequestBody AppointmentRequestDTO appointmentRequestDTO) {
-        return ResponseEntity.ok("Appointment Details Updated!");
+    public ResponseEntity<String> updateAppointmentDetails(@RequestBody Appointment appointment) {
+        return appointmentService.updateAppointmentDetails(appointment);
     }
 
     @PutMapping("/update/status")
-    public ResponseEntity<String> updateAppointmentStatus(@RequestBody AppointmentRequestDTO appointmentRequestDTO) {
-        return ResponseEntity.ok("Appointment Status Updated!");
+    public ResponseEntity<String> updateAppointmentStatus(@RequestBody Appointment appointment) {
+        return appointmentService.updateAppointmentStatus(appointment);
     }
 }
--- med-booking-backend/src/main/java/com/health_care/med_booking_backend/service/AppointmentService.java
+++ med-booking-backend/src/main/java/com/health_care/med_booking_backend/service/AppointmentService.java
@@ -1,2 +1,55 @@
-package com.health_care.med_booking_backend.service;public class AppointmentService {
+package com.health_care.med_booking_backend.service;
+
+import com.health_care.med_booking_backend.dto.AppointmentRequestDTO;
+import com.health_care.med_booking_backend.model.Appointment;
+import com.health_care.med_booking_backend.repository.AppointmentRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+    private final AppointmentRepository appointmentRepository;
+
+    public AppointmentService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public ResponseEntity<String> createNewAppointment(Appointment appointment) {
+        // grab patient & doctor entities
+
+        // check that appointment doesn't exist OR IS CANCELED
+
+        // validate appointment conditions (if false return "misssing XX" detail
+        // -- date is in the future
+        // -- all fields provided (patient, doctor, date & time, visitType, automatically set to BOOKED)
+        // -- no more than one appointment per day, per patient
+
+
+
+        // if all is well, save appointment to the database!
+
+
+        return ResponseEntity.ok("Appointment Created!");
+    }
+
+    public ResponseEntity<String> updateAppointmentDetails(Appointment appointment) {
+
+        // check that appointment exists
+
+        // check for change to appointment date
+
+        // check for change to appointment doctor & doctor validations
+
+        // check for change to visitType
+
+        // check for change to status
+
+
+        return ResponseEntity.ok("Appointment Details Updated!");
+    }
+
+    public ResponseEntity<String> updateAppointmentStatus(Appointment appointment) {
+        return ResponseEntity.ok("Appointment Status Updated!");
+    }
+
 }

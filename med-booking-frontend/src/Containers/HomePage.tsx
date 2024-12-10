2f47be78ee8129f7eb9650a69e8321fd61a9796d
Peder Goodman <peder.goodman@gmail.com>
2024-12-10 17:15:09 -0600

    home page logic to display signin or signup forms

--- med-booking-frontend/src/Containers/HomePage.tsx
+++ med-booking-frontend/src/Containers/HomePage.tsx
@@ -1,8 +1,11 @@
 // aka login
 
-import { useEffect } from "react";
+import { Card } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { LoginForm } from "../Components/forms/LoginForm";
+import { SignupForm } from "../Components/forms/SignupForm";
 import { checkUserAuthentication } from "../store/actions/userActions";
 import { store } from "../store/store";
 
@@ -11,7 +14,9 @@ export const HomePage = () => {
   const { userDetails, isUserAuthenticated } = useSelector(
     (state: RootState) => state.user
   );
+
   const userRole = userDetails?.role;
+  const [showSignup, setShowSignup] = useState(false);
 
   useEffect(() => {
     store.dispatch(checkUserAuthentication());
@@ -27,11 +32,48 @@ export const HomePage = () => {
     }
   }, [isUserAuthenticated, userRole]);
 
+  const loginCardStyling = {
+    maxWidth: "400px",
+    margin: "60px auto 20px",
+    padding: "30px",
+    boxShadow: "0px 0px 30px 5px rgba(0,0,0,0.14)",
+  };
+
+  const ExistingUserPrompt = () => {
+    return (
+      <p>
+        New here? <button onClick={() => setShowSignup(true)}>Sign-up</button>
+      </p>
+    );
+  };
+
+  const NewUserPrompt = () => {
+    return (
+      <p>
+        Existing User?{" "}
+        <button onClick={() => setShowSignup(false)}>Sign-in</button>
+      </p>
+    );
+  };
+
+  const termsMessaging = showSignup ? "signing up" : "signing in";
+
   return (
     <>
-      <div>HomePage</div>
-      <div>Looks like you're not logged in!!</div>
-      <div>Please Login!</div>
+      <Card sx={loginCardStyling}>
+        <h1>Patient Sign-in</h1>
+        {showSignup ? <NewUserPrompt /> : <ExistingUserPrompt />}
+        {showSignup ? <SignupForm /> : <LoginForm />}
+        <p>
+          By {termsMessaging}, you agree to accept our{" "}
+          <a
+            href="https://genius.com/Rick-astley-never-gonna-give-you-up-lyrics"
+            target="_blank"
+          >
+            terms and conditions.
+          </a>
+        </p>
+      </Card>
     </>
   );
 };

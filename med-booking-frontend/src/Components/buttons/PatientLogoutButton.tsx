d72c69ff0ec55a5084856b16eb06799ecababbb1
Peder Goodman <peder.goodman@gmail.com>
2024-12-10 22:36:24 -0600

    update PatientLogoutButton

--- med-booking-frontend/src/Components/buttons/PatientLogoutButton.tsx
+++ med-booking-frontend/src/Components/buttons/PatientLogoutButton.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@mui/material";
-import { logoutUser } from "../../store/actions/userActions";
-import { store } from "../../store/store";
-import { resetUserState } from "../../store/reducers/userReducer";
 import { useDispatch } from "react-redux";
+import { resetUserState } from "../../store/reducers/userReducer";
 
 export const PatientLogoutButton = () => {
   const dispatch = useDispatch();

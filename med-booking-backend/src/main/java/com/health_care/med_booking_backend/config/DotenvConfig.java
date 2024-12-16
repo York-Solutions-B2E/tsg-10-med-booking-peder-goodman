b9c379106364f025f89c032b105d52bca89a7780
Peder Goodman <peder.goodman@gmail.com>
2024-12-16 10:19:00 -0600

    add dotenv dependency to project

---
+++ med-booking-backend/src/main/java/com/health_care/med_booking_backend/config/DotenvConfig.java
@@ -0,0 +1,14 @@
+package com.health_care.med_booking_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class DotenvConfig {
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.configure().ignoreIfMissing().load();
+    }
+}

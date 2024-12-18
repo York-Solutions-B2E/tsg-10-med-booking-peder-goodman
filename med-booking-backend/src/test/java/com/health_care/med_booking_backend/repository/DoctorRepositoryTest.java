01e8ff75f4a8aafaf36d5ea7c9acf854a4ec4b8a
Peder Goodman <peder.goodman@gmail.com>
2024-12-18 13:42:01 -0600

    update DoctorRepositoryTest to use mocks

--- med-booking-backend/src/test/java/com/health_care/med_booking_backend/repository/DoctorRepositoryTest.java
+++ med-booking-backend/src/test/java/com/health_care/med_booking_backend/repository/DoctorRepositoryTest.java
@@ -1,16 +1,21 @@
 package com.health_care.med_booking_backend.repository;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.health_care.med_booking_backend.model.Appointment;
 import com.health_care.med_booking_backend.model.AppointmentStatus;
@@ -18,19 +23,18 @@ import com.health_care.med_booking_backend.model.Doctor;
 import com.health_care.med_booking_backend.model.Patient;
 import com.health_care.med_booking_backend.model.Specialization;
 import com.health_care.med_booking_backend.model.VisitType;
-import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @DataJpaTest
 public class DoctorRepositoryTest {
 
-    @Autowired
+    @Mock
     private DoctorRepository doctorRepository;
-    @Autowired
+    @Mock
     private SpecializationRepository specializationRepository;
-    @Autowired
+    @Mock
     private AppointmentRepository appointmentRepository;
-    @Autowired
+    @Mock
     private PatientRepository patientRepository;
 
     private Doctor doctor1;
@@ -42,26 +46,22 @@ public class DoctorRepositoryTest {
 
     @BeforeEach
     void setUp() {
-        // Clear the repository before each test
-        doctorRepository.deleteAll();
-        specializationRepository.deleteAll();
-        appointmentRepository.deleteAll();
-        patientRepository.deleteAll();
+        MockitoAnnotations.openMocks(this);
 
         // Add common test data
         cardiology = new Specialization("Cardiology");
         orthopedics = new Specialization("Orthopedics");
-        specializationRepository.save(cardiology);
-        specializationRepository.save(orthopedics);
-
         doctor1 = new Doctor("John", "Doe", cardiology);
         doctor2 = new Doctor("Jane", "Smith", orthopedics);
-        doctorRepository.save(doctor1);
-        doctorRepository.save(doctor2);
         patient1 = new Patient("Alice", "Brown", "alice@gmail.com", LocalDate.parse("1990-07-12"));
         patient2 = new Patient("Corey", "Goodman", "corey@gmail.com", LocalDate.parse("1988-12-22"));
-        patientRepository.save(patient1);
-        patientRepository.save(patient2);
+
+        when(specializationRepository.save(cardiology)).thenReturn(cardiology);
+        when(specializationRepository.save(orthopedics)).thenReturn(orthopedics);
+        when(doctorRepository.save(doctor1)).thenReturn(doctor1);
+        when(doctorRepository.save(doctor2)).thenReturn(doctor2);
+        when(patientRepository.save(patient1)).thenReturn(patient1);
+        when(patientRepository.save(patient2)).thenReturn(patient2);
     }
 
     void setUpAppointments() {
@@ -72,16 +72,22 @@ public class DoctorRepositoryTest {
                 VisitType.IN_PERSON, AppointmentStatus.CANCELED);
         Appointment appointment3 = new Appointment(patient1, doctor1, LocalDate.now().plusDays(2), LocalTime.now(),
                 VisitType.IN_PERSON, AppointmentStatus.BOOKED);
-        appointmentRepository.save(appointment1);
-        appointmentRepository.save(appointment2);
-        appointmentRepository.save(appointment3);
+        when(appointmentRepository.save(appointment1)).thenReturn(appointment1);
+        when(appointmentRepository.save(appointment2)).thenReturn(appointment2);
+        when(appointmentRepository.save(appointment3)).thenReturn(appointment3);
+        when(doctorRepository.findAppointmentsByDoctorIdAndNotBooked(doctor1.getId()))
+                .thenReturn(Arrays.asList(appointment1, appointment3));
     }
 
     // * Test the findAppointmentsByDoctorIdAndNotBooked method
     @Test
     void testFindAppointmentsByDoctorIdAndNotBooked_returnsEmptyList() {
+        // Arrange
+        when(doctorRepository.findAppointmentsByDoctorIdAndNotBooked(doctor2.getId()))
+                .thenReturn(Collections.emptyList());
+
         // Act
-        List<Appointment> appointments = doctorRepository.findAppointmentsByDoctorIdAndNotBooked(doctor1.getId());
+        List<Appointment> appointments = doctorRepository.findAppointmentsByDoctorIdAndNotBooked(doctor2.getId());
 
         // Assert
         assertThat(appointments).isEmpty(); // Assuming no appointments are booked initially
@@ -101,13 +107,16 @@ public class DoctorRepositoryTest {
         assertThat(appointments).extracting(Appointment::getAppointmentStatus)
                 .doesNotContain(AppointmentStatus.CANCELED);
         assertThat(appointments).extracting(Appointment::getAppointmentStatus).containsOnly(AppointmentStatus.BOOKED);
-
     }
 
     // * Test the findDoctorByFirstNameAndLastNameAndSpecialization method
     // test that doctor is found
     @Test
     void testFindDoctorByFirstNameAndLastNameAndSpecialization_returnsDoctor() {
+        // Arrange
+        when(doctorRepository.findDoctorByFirstNameAndLastNameAndSpecialization("John", "Doe", cardiology))
+                .thenReturn(Optional.of(doctor1));
+
         // Act
         Optional<Doctor> foundDoctor = doctorRepository.findDoctorByFirstNameAndLastNameAndSpecialization(
                 "John", "Doe", cardiology);
@@ -122,6 +131,9 @@ public class DoctorRepositoryTest {
     // test that doctor is not found
     @Test
     void testFindDoctorByFirstNameAndLastNameAndSpecialization_returnsIsEmpty() {
+        // Arrange
+        when(doctorRepository.findDoctorByFirstNameAndLastNameAndSpecialization("Jermey", "Smith", cardiology))
+                .thenReturn(Optional.empty());
         // Act
         Optional<Doctor> foundDoctor = doctorRepository.findDoctorByFirstNameAndLastNameAndSpecialization(
                 "Jermey", "Smith", cardiology);

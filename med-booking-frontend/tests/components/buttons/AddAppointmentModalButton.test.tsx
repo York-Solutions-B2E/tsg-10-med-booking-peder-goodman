a1805cdea5670afbb75e59307f0793623e888039
Peder Goodman <peder.goodman@gmail.com>
2024-12-21 13:08:57 -0600

    added tests for form opener modals

--- med-booking-frontend/tests/components/buttons/AddAppointmentModalButton.test.tsx
+++ med-booking-frontend/tests/components/buttons/AddAppointmentModalButton.test.tsx
@@ -22,8 +22,7 @@ describe("AddAppointmentModalButton Form Open and Cancel", () => {
     );
     fireEvent.click(screen.getByText("Add Appointment"));
     expect(screen.getByText("Create Appointment")).toBeInTheDocument();
-    expect(screen.getByText("Patient Details")).toBeInTheDocument();
-    expect(screen.getByText("Confirm Appointment")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /Confirm Appointment/i })).toBeInTheDocument();
   });
 
   test("opens the cancel confirmation modal on cancel submission", () => {
@@ -33,30 +32,31 @@ describe("AddAppointmentModalButton Form Open and Cancel", () => {
       </Provider>
     );
     fireEvent.click(screen.getByText("Add Appointment"));
-    // ? SWEEET the data-testId is included in the MUI component
     fireEvent.click(screen.getByTestId("CloseIcon"));
+
     expect(screen.getByText("Are you sure you want to cancel?")).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /cancel/i })).toBeVisible();
     expect(screen.getByRole("button", { name: /yes/i })).toBeVisible();
   });
 
-  test("closes the cancel confirmation modal on cancel the cancel modal", () => {
+  test("closes the cancel confirmation modal only, aka: cancel the cancel", async () => {
     render(
       <Provider store={store}>
         <AddAppointmentModalButton />
       </Provider>
     );
 
-    expect(screen.queryByText("Create Appointment")).not.toBeInTheDocument();
-
     // open the form modal, then try to cancel it
     fireEvent.click(screen.getByText("Add Appointment"));
     fireEvent.click(screen.getByTestId("CloseIcon"));
     // cancel the cancel modal
-    fireEvent.click(screen.getByText("Yes"));
+    fireEvent.click(screen.getByText("Cancel"));
 
-    expect(screen.queryByText("Are you sure you want to cancel?")).not.toBeVisible();
-    expect(screen.queryByText("Create Appointment")).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.queryByText("Are you sure you want to cancel?")).not.toBeInTheDocument();
+      expect(screen.queryByText("Create Appointment")).toBeInTheDocument();
+      expect(screen.queryByRole("button", { name: /Confirm Appointment/i })).toBeInTheDocument();
+    });
   });
 
   test("closes the form modal and cancel modal on confirm the cancel modal", async () => {
@@ -75,41 +75,7 @@ describe("AddAppointmentModalButton Form Open and Cancel", () => {
     await waitFor(() => {
       expect(screen.queryByText("Are you sure you want to cancel?")).not.toBeInTheDocument();
       expect(screen.queryByText("Create Appointment")).not.toBeInTheDocument();
+      expect(screen.queryByRole("button", { name: /Confirm Appointment/i })).not.toBeInTheDocument();
     });
-  });
-});
-
-//
-describe("AddAppointmentModalButton Form Submission modal", () => {
-  // jest.mock("../../../src/Components/modals/ConfirmationAppointmentModal", () => <div>ConfirmationAppointmentModal</div>);
-
-  test.skip("opens the confirmation modal on form submission", () => {
-    render(
-      <Provider store={store}>
-        <AddAppointmentModalButton />
-      </Provider>
-    );
-
-    // Open the form modal
-    fireEvent.click(screen.getByText("Add Appointment"));
-
-    // Mock the form submission handler to directly open the confirmation modal
-    fireEvent.click(screen.getByText("Confirm Appointment"));
-
-    // Check if the confirmation modal is open
-    expect(screen.getByText("ConfirmationAppointmentModal")).toBeInTheDocument();
-  });
-
-  test.skip("dispatches actions on confirm submission", async () => {
-    render(
-      <Provider store={store}>
-        <AddAppointmentModalButton />
-      </Provider>
-    );
-    fireEvent.click(screen.getByText("Add Appointment"));
-    fireEvent.submit(screen.getByText("Confirm Appointment"));
-    fireEvent.click(screen.getByText("Submit"));
-    // Add assertions to check if the appropriate actions are dispatched
-    expect(screen.queryByText("ConfirmationAppointmentModal")).not.toBeInTheDocument();
   });
 });
--- med-booking-frontend/tests/components/buttons/AddDoctorModalButton.test.tsx
+++ med-booking-frontend/tests/components/buttons/AddDoctorModalButton.test.tsx
@@ -1,5 +1,80 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { Provider } from "react-redux";
+import AddDoctorModalButton from "../../../src/Components/buttons/AddDoctorModalButton";
+import { store } from "../../../src/store/store";
+
 export {};
 
 describe("AddDoctorModalButton Form Open and Cancel", () => {
-  test.skip("renders the Add Doctor button", () => {});
+  test("renders the Add Doctor button", () => {
+    render(
+      <Provider store={store}>
+        <AddDoctorModalButton />
+      </Provider>
+    );
+    expect(screen.getByText("Add New Doctor")).toBeInTheDocument();
+  });
+
+  test("opens the form modal when Add New Doctor button is clicked", () => {
+    render(
+      <Provider store={store}>
+        <AddDoctorModalButton />
+      </Provider>
+    );
+    fireEvent.click(screen.getByText("Add New Doctor"));
+    expect(screen.getByRole("button", { name: /Add Doctor/i })).toBeInTheDocument();
+  });
+
+  test("opens the cancel confirmation modal on cancel submission", () => {
+    render(
+      <Provider store={store}>
+        <AddDoctorModalButton />
+      </Provider>
+    );
+
+    fireEvent.click(screen.getByText("Add New Doctor"));
+    fireEvent.click(screen.getByTestId("CloseIcon"));
+
+    expect(screen.getByText("Are you sure you want to cancel?")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: /cancel/i })).toBeVisible();
+    expect(screen.getByRole("button", { name: /yes/i })).toBeVisible();
+  });
+
+  test("closes the cancel confirmation modal only, aka: cancel the cancel", async () => {
+    render(
+      <Provider store={store}>
+        <AddDoctorModalButton />
+      </Provider>
+    );
+
+    // open the form modal, then try to cancel it
+    fireEvent.click(screen.getByText("Add New Doctor"));
+    fireEvent.click(screen.getByTestId("CloseIcon"));
+    // cancel the cancel modal
+    fireEvent.click(screen.getByText("Cancel"));
+
+    await waitFor(() => {
+      expect(screen.queryByRole("button", { name: /Add Doctor/i })).toBeInTheDocument();
+      expect(screen.queryByText("Are you sure you want to cancel?")).not.toBeInTheDocument();
+    });
+  });
+
+  test("closes the form modal and cancel modal on confirm the cancel modal", async () => {
+    render(
+      <Provider store={store}>
+        <AddDoctorModalButton />
+      </Provider>
+    );
+
+    // open the form modal, then try to cancel it
+    fireEvent.click(screen.getByText("Add New Doctor"));
+    fireEvent.click(screen.getByTestId("CloseIcon"));
+    // cancel the cancel modal
+    fireEvent.click(screen.getByText("Yes"));
+
+    await waitFor(() => {
+      expect(screen.queryByText("Are you sure you want to cancel?")).not.toBeInTheDocument();
+      expect(screen.queryByRole("button", { name: /Add Doctor/i })).not.toBeInTheDocument();
+    });
+  });
 });

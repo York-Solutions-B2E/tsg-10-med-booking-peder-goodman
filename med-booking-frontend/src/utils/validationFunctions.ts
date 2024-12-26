5117f75d1ff2b23e75d7b884725843c6690359d1
Peder Goodman <peder.goodman@gmail.com>
2024-12-26 14:14:55 -0600

    update DateTime validations past/future

--- med-booking-frontend/src/utils/validationFunctions.ts
+++ med-booking-frontend/src/utils/validationFunctions.ts
@@ -12,30 +12,59 @@ const validateEmail = (email: string) => {
 
 const validateDateIsInPast = (selectedDateTime: Dayjs | null) => {
   const currentDateTime = dayjs();
+  const isSelectedDateInThePast = selectedDateTime?.isBefore(currentDateTime, "day");
 
-  if (!selectedDateTime) {
+  // if empty string false
+  if (selectedDateTime === null) {
     return false;
   }
 
-  if (selectedDateTime < currentDateTime) {
+  //  if selected date is in the past true
+  if (isSelectedDateInThePast) {
     return true;
   }
 
+  // if selected date is today true
   return false;
 };
 
 const validateDateIsInFuture = (selectedDate: Dayjs | null) => {
-  const currentDate = dayjs();
+  const currentDateTime = dayjs();
+  const isSelectedDateInThePast = selectedDate?.isAfter(currentDateTime, "day");
+  const isSelectedDateToday = selectedDate?.isSame(currentDateTime, "day");
 
-  if (!selectedDate) {
+  // if empty string false
+  if (selectedDate === null) {
     return false;
   }
 
-  if (selectedDate > currentDate) {
+  // if selected date is in the future, true
+  if (isSelectedDateInThePast) {
+    return true;
+  }
+
+  // if selected date is today, true
+  if (isSelectedDateToday) {
+    return true;
+  }
+
+  // if selected date is in the past, false
+  return false;
+};
+
+const validateTimeIsInFuture = (selectedDateTime: Dayjs | null) => {
+  const currentDateTime = dayjs();
+
+  // if null, false
+  if (selectedDateTime === null) {
+    return false;
+  }
+
+  if (selectedDateTime.isAfter(currentDateTime.endOf("hour"))) {
     return true;
   }
 
   return false;
 };
 
-export { validateDateIsInFuture, validateDateIsInPast, validateEmail };
+export { validateDateIsInFuture, validateDateIsInPast, validateEmail, validateTimeIsInFuture };

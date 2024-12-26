43fcfc8b70d65a1bd0bf775a0d8d61678682a81c
Peder Goodman <peder.goodman@gmail.com>
2024-12-26 14:21:01 -0600

    update AppointmentDataGrid to manage clicks and action submissions

--- med-booking-frontend/src/Components/data-display/AppointmentDataGrid.tsx
+++ med-booking-frontend/src/Components/data-display/AppointmentDataGrid.tsx
@@ -1,20 +1,54 @@
+import DoNotDisturbIcon from "@mui/icons-material/DoNotDisturb";
+import EditIcon from "@mui/icons-material/Edit";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import { DataGrid, GridColDef, GridRowParams } from "@mui/x-data-grid";
 import dayjs from "dayjs";
-import { useEffect } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import { calculateAge } from "../../utils/helperFunctions";
-import CancelAppointmentModalButton from "../buttons/CancelAppointmentModalButton";
-import EditAppointmentModalButton from "../buttons/EditAppointmentModalButton";
+import { cancelAppointment } from "../../store/actions/appointmentActions";
+import { getPatientDetails } from "../../store/actions/userActions";
+import { store } from "../../store/store";
+import { calculateAge, checkIfAppointmentIsEditable } from "../../utils/helperFunctions";
+import { ButtonIconWithToolTip } from "../buttons/ButtonIconWithToolTip";
+import { AppointmentForm } from "../forms/AppointmentForm";
+import { ConfirmActionModal } from "../modals/ConfirmActionModal";
+import { FormModal } from "../modals/FormModal";
+import { useCustomModal } from "../modals/useCustomModal";
 
 // ******** FUNCTION START
 export default function AppointmentDataGrid() {
-  // const [isConfirmDeleteOpen, openConfirmDelete, closeConfirmDelete] = useCustomModal();
-  //   const [isEditDoctorFormOpen, openEditDoctorFormModal, closeEditDoctorFormModal] = useCustomModal();
-  //   const [selectedDoctor, setSelectedDoctor] = useState<DoctorDetails | undefined>(undefined);
   const patientDetails = useSelector((state: RootState) => state.user.userDetails as PatientDetails);
   const { patientAppointments } = patientDetails;
+
+  const [isConfirmCancelAppointmentOpen, openConfirmCancelAppointment, closeConfirmCancelAppointment] = useCustomModal();
+  const [isEditAppointmentFormOpen, openEditAppointmentFormModal, closeEditAppointmentFormModal] = useCustomModal();
+  const [selectedAppointment, setSelectedAppointment] = useState<Appointment | undefined>(undefined);
+
+  // * click handlers
+  const handleClickEdit = (appointment: Appointment) => {
+    setSelectedAppointment(appointment);
+    openEditAppointmentFormModal();
+  };
+
+  const handleClickCancelAppointment = (appointment: Appointment) => {
+    setSelectedAppointment(appointment);
+    openConfirmCancelAppointment();
+  };
+
+  const handleConfirmCancelAppointment = async () => {
+    if (!selectedAppointment) {
+      console.error("selectedAppointment is undefined");
+      return;
+    }
+
+    // Cancel the appointment and refresh the patient details
+    await store.dispatch(cancelAppointment(selectedAppointment.id));
+    store.dispatch(getPatientDetails(selectedAppointment.patient.id as number));
+
+    closeConfirmCancelAppointment();
+    setSelectedAppointment(undefined);
+  };
 
   // ******** Columns headers and data
   const columns: GridColDef[] = [
@@ -82,7 +116,7 @@ export default function AppointmentDataGrid() {
         return (
           <Box>
             <Typography sx={{ fontSize: 14 }}> {appointmentDateTime.format("MMM D, YY")}</Typography>
-            <Typography sx={{ fontSize: 16 }}>{appointmentDateTime.format("HH:mma")}</Typography>
+            <Typography sx={{ fontSize: 16 }}>{appointmentDateTime.format("h:mma")}</Typography>
           </Box>
         );
       },
@@ -94,7 +128,7 @@ export default function AppointmentDataGrid() {
       width: 120,
       renderCell: (params: { row: Appointment }) => {
         const status = params.row.appointmentStatus;
-        const appointmentDateTime = dayjs(params.row.appointmentDate, params.row.appointmentTime);
+        const appointmentDateTime = dayjs(params.row.appointmentDate + params.row.appointmentTime);
         const currentDateTime = dayjs();
 
         // if it's not canceled and the appointment date is in the past, then it's completed
@@ -119,16 +153,31 @@ export default function AppointmentDataGrid() {
       cellClassName: "actions",
       getActions: (params: GridRowParams) => {
         const appointment = params.row as Appointment;
+        const isAppointmentEditable = checkIfAppointmentIsEditable(appointment);
 
-        return [<EditAppointmentModalButton appointment={appointment} />, <CancelAppointmentModalButton appointment={appointment} />];
+        return [
+          <ButtonIconWithToolTip
+            disabled={!isAppointmentEditable}
+            onClick={() => {
+              handleClickEdit(appointment);
+            }}
+            icon={<EditIcon />}
+            tooltip="Edit Appointment"
+          />,
+          <ButtonIconWithToolTip
+            disabled={!isAppointmentEditable}
+            color="error"
+            tooltip="Cancel Appointment"
+            onClick={() => {
+              handleClickCancelAppointment(appointment);
+            }}
+            icon={<DoNotDisturbIcon />}
+          />,
+        ];
       },
     },
   ];
   // END ******** Columns headers and data
-
-  useEffect(() => {
-    console.log("patientAppointments", patientAppointments);
-  }, [patientAppointments]);
 
   return (
     <Box sx={{ height: 700, width: "100%" }}>
@@ -145,6 +194,23 @@ export default function AppointmentDataGrid() {
         pageSizeOptions={[10, 25, 50]}
         disableRowSelectionOnClick
         // slots={{ noRowsOverlay: CustomNoRowsOverlay }}
+      />
+      <FormModal
+        open={isEditAppointmentFormOpen}
+        onClose={closeEditAppointmentFormModal}
+        maxWidth={"sm"}
+        FormComponent={AppointmentForm}
+        isEditing={true}
+        formData={selectedAppointment}
+      />
+
+      <ConfirmActionModal
+        color="error"
+        message="Are you sure you want to cancel this appointment?"
+        open={isConfirmCancelAppointmentOpen}
+        onDismiss={closeConfirmCancelAppointment}
+        onConfirmAction={handleConfirmCancelAppointment}
+        confirmButtonText="Yes"
       />
     </Box>
   );
